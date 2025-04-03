40def1b78892cee899d8e834d09fcffe94f37e61
Manavshahoff <manavshah.off@gmail.com>
2025-04-03 01:15:54 -0400

    Changed the fetching quote functions

--- doc/sprint1/sprint1Frontend/.next/server/webpack-runtime.js
+++ doc/sprint1/sprint1Frontend/.next/server/webpack-runtime.js
@@ -47,11 +47,6 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
-/******/ 	/* webpack/runtime/amd options */
-/******/ 	(() => {
-/******/ 		__webpack_require__.amdO = {};
-/******/ 	})();
-/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -130,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("0cdca24f482b27f0")
+/******/ 		__webpack_require__.h = () => ("5bdde2296882f678")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- doc/sprint1/sprint1Frontend/.next/static/chunks/webpack.js
+++ doc/sprint1/sprint1Frontend/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2a828f9c918357f5"; }
+/******/ 		__webpack_require__.h = function() { return "09965179f089df80"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
